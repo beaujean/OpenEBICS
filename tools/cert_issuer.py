9d6e4388779057b8745e39507c0b9b204d2e2b76
Aurelien Beaujean <abeaujean@corp.free.fr>
2016-07-19 16:54:37 +0200

    A005 Certificates

--- tools/cert_issuer.py
+++ tools/cert_issuer.py
@@ -1,4 +1,5 @@
 import OpenSSL.crypto
+from Crypto.Util import asn1
 import dumper
 import pprint
 
@@ -18,3 +19,21 @@ print (iss.O)
 print (key.type())
 print (key.bits())
 
+print (cert.get_serial_number())
+
+pub = cert.get_pubkey()
+
+# Only works for RSA (I think)
+if pub.type()!=c.TYPE_RSA:
+    raise Exception('Can only handle RSA keys')
+
+# This seems to work with public as well
+pub_asn1=c.dump_privatekey(c.FILETYPE_ASN1, pub)
+# Decode DER
+pub_der=asn1.DerSequence()
+pub_der.decode(pub_asn1)
+# Get the modulus
+pub_modulus=pub_der[1]
+pub_exponent=pub_der[2]
+print (pub_modulus)
+print (pub_exponent)
