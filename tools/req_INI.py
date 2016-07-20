becb42f51c60b676545001bb316d373d4206642b
Aurelien Beaujean <abeaujean@corp.free.fr>
2016-07-20 17:13:46 +0200

    use lib and fix modulus and exp

--- tools/req_INI.py
+++ tools/req_INI.py
@@ -11,6 +11,8 @@ import OpenSSL.crypto
 from Crypto.Util import asn1
 import xml.etree.ElementTree as ET
 from jinja2 import Environment, FileSystemLoader
+sys.path.append('./libs/')
+import OpenEBICS.certs as OEcert
 
 if sys.argv and sys.argv[1:]:
     cfgfile = sys.argv[1]
@@ -33,32 +35,11 @@ for user in cfg['Users']:
         User = user
         UserID = cfg['Users'][user]['UserID']
 
-# Open "auth" certificate file
-c = OpenSSL.crypto
-st_cert = open('certs/'+User+'/auth.crt', 'rt').read()
-cert = c.load_certificate(c.FILETYPE_PEM, st_cert)
+# Getting useful certificate informations
+auth_cert = OEcert.get_cert_info('certs/'+User+'/auth.crt')
 
-# Format cert string as EBICS needs it
-st_cert = st_cert.replace('-----BEGIN CERTIFICATE-----', '')
-st_cert = st_cert.replace('-----END CERTIFICATE-----', '')
-st_cert = st_cert.replace("\n", '')
-
-# Get cert infos
-Issuer = cert.get_issuer()
-Pubkey = cert.get_pubkey()
-SerialNumber = cert.get_serial_number()
-
-# Get Modulus and Exponent from X509 object
-pub_asn1 = c.dump_privatekey(c.FILETYPE_ASN1, Pubkey)
-# Decode DER
-pub_der = asn1.DerSequence()
-pub_der.decode(pub_asn1)
-# Finally modulus / Exponent !
-Modulus = pub_der[1]
-Exponent = pub_der[2]
-
-Modulus = 't/ukB4IaOuQfPBEFW+HGoHOmsiSFbH1jVx7kVF6e+iU2w9LLktQqZPvHZV4eA71mdJmsrvmXe78sg8Bx1kUuI0MduWTHsS9AedZUel1AHMZ14+KPUyGMQZBjNVM+Pbj279mykyCJzJz7MOfn9+ppU8Lj6Xrd6E0YTIxGusx0YUF33vDm0t0rM2e9Mfj+2KRkdeu86CCBTUsC6x9+79h9glo7zrm1IE53vqcKfVCAwexjYtha7NgNnpY+QZjvfAYhDuqN3hDtXN6igus2TVHvEwWd7P8NYVQrRfDIugFkHJ/S7bCiFVeSbjbaDELGf7KlALW8YpIh91MBBWyehqRUzw=='
-Exponent = 'AQAB'
+#Modulus = 't/ukB4IaOuQfPBEFW+HGoHOmsiSFbH1jVx7kVF6e+iU2w9LLktQqZPvHZV4eA71mdJmsrvmXe78sg8Bx1kUuI0MduWTHsS9AedZUel1AHMZ14+KPUyGMQZBjNVM+Pbj279mykyCJzJz7MOfn9+ppU8Lj6Xrd6E0YTIxGusx0YUF33vDm0t0rM2e9Mfj+2KRkdeu86CCBTUsC6x9+79h9glo7zrm1IE53vqcKfVCAwexjYtha7NgNnpY+QZjvfAYhDuqN3hDtXN6igus2TVHvEwWd7P8NYVQrRfDIugFkHJ/S7bCiFVeSbjbaDELGf7KlALW8YpIh91MBBWyehqRUzw=='
+#Exponent = 'AQAB'
 
 # 2016-07-07T13:26:01.592+01:00
 TimeStamp = datetime.datetime.now(tz=pytz.timezone('Europe/Paris')).isoformat('T')
@@ -67,12 +48,12 @@ TimeStamp = datetime.datetime.now(tz=pytz.timezone('Europe/Paris')).isoformat('T
 xml_A005 = Tpl_A005.render(HostID = cfg['Server']['HostID'],
                         PartnerID = cfg['Server']['PartnerID'],
                         UserID = UserID,
-                        IssuerName = Issuer.CN,
-                        SerialNumber = SerialNumber,
-                        Certificate = st_cert,
                         TimeStamp = TimeStamp,
-                        Modulus = Modulus,
-                        Exponent = Exponent)
+                        IssuerName = auth_cert['Issuer'],
+                        SerialNumber = auth_cert['SerialNumber'],
+                        Certificate = auth_cert['Cert'],
+                        Modulus = auth_cert['Modulus'],
+                        Exponent = auth_cert['Exponent'])
 print (xml_A005)
 
 # Gzip and base64 A005 auth cert
