efd79ca2c48979c65e931e9ce5bb500cbeeff014
Aurelien Beaujean <abeaujean@corp.free.fr>
2016-07-20 17:13:50 +0200

    use lib and fix modulus and exp

---
+++ tools/req_HIA.py
@@ -0,0 +1,96 @@
+import sys
+import yaml
+import zlib
+import pytz
+import base64
+import pprint
+import datetime
+from datetime import time, tzinfo
+import requests
+import xml.etree.ElementTree as ET
+from jinja2 import Environment, FileSystemLoader
+sys.path.append('./libs/')
+import OpenEBICS.certs as OEcert
+
+if sys.argv and sys.argv[1:]:
+    cfgfile = sys.argv[1]
+else:
+    cfgfile = 'ebics.yml'
+
+# Loading the EBICS config file
+with open(cfgfile, 'r') as ymlfile:
+    cfg = yaml.load(ymlfile)
+
+# Parsing users args
+for user in cfg['Users']:
+    if 'transport' in cfg['Users'][user]:
+        print ('Transporter:',user,'->',cfg['Users'][user]['UserID'])
+        User = user
+        UserID = cfg['Users'][user]['UserID']
+
+# Getting useful certificate informations
+auth_cert = OEcert.get_cert_info('certs/'+User+'/auth.crt')
+encr_cert = OEcert.get_cert_info('certs/'+User+'/crypt.crt')
+
+pprint.pprint(auth_cert)
+pprint.pprint(encr_cert)
+
+Modulus = 't/ukB4IaOuQfPBEFW+HGoHOmsiSFbH1jVx7kVF6e+iU2w9LLktQqZPvHZV4eA71mdJmsrvmXe78sg8Bx1kUuI0MduWTHsS9AedZUel1AHMZ14+KPUyGMQZBjNVM+Pbj279mykyCJzJz7MOfn9+ppU8Lj6Xrd6E0YTIxGusx0YUF33vDm0t0rM2e9Mfj+2KRkdeu86CCBTUsC6x9+79h9glo7zrm1IE53vqcKfVCAwexjYtha7NgNnpY+QZjvfAYhDuqN3hDtXN6igus2TVHvEwWd7P8NYVQrRfDIugFkHJ/S7bCiFVeSbjbaDELGf7KlALW8YpIh91MBBWyehqRUzw=='
+Exponent = 'AQAB'
+
+print ("\nMod:",Modulus,"\nExp:",Exponent,"\n")
+
+# 2016-07-07T13:26:01.592+01:00
+TimeStamp = datetime.datetime.now(tz=pytz.timezone('Europe/Paris')).isoformat('T')
+
+# Opening templates
+TplEnv = Environment(loader=FileSystemLoader('xml/'))
+Tpl_HIA = TplEnv.get_template('HIA.xml')
+Tpl_HIA_data = TplEnv.get_template('HIA-data.xml')
+
+# Parsing A005 templates
+xml_HIA_data = Tpl_HIA_data.render(HostID = cfg['Server']['HostID'],
+                        PartnerID = cfg['Server']['PartnerID'],
+                        UserID = UserID,
+                        AuthIssuerName = auth_cert['Issuer'],
+                        AuthSerialNumber = auth_cert['SerialNumber'],
+                        AuthCertificate = auth_cert['Cert'],
+                        AuthModulus = auth_cert['Modulus'],
+                        AuthExponent = auth_cert['Exponent'],
+                        EncrIssuerName = encr_cert['Issuer'],
+                        EncrSerialNumber = encr_cert['SerialNumber'],
+                        EncrCertificate = encr_cert['Cert'],
+                        EncrModulus = encr_cert['Modulus'],
+                        EncrExponent = encr_cert['Exponent'],
+                        TimeStamp = TimeStamp)
+print (xml_HIA_data)
+
+sys.exit()
+
+# Gzip and base64 HIA data auth cert
+zip_HIA_data = zlib.compress(xml_HIA_data.encode())
+b64_HIA_data = base64.b64encode(zip_HIA_data)
+
+# Parsing HIA templates
+xml_HIA = Tpl_HIA.render(HostID = cfg['Server']['HostID'],
+                        PartnerID = cfg['Server']['PartnerID'],
+                        UserID = UserID,
+                        OrderID = 'A001',
+                        OrderData = b64_HIA_data.decode())
+print (xml_HIA)
+
+#if 'Cert' in cfg['Server']:
+#    response = requests.post(cfg['Server']['URL'], cert=cfg['Server']['Cert'], data=xml_HIA.encode())
+#else:
+#    response = requests.post(cfg['Server']['URL'], xml_HIA.encode())
+#
+#print (response.text)
+
+#ebixml = ET.fromstring(response.text)
+#
+##for child in ebixml:
+##    print (child.tag,' -> ',child.attrib, ' txt: ', child.text)
+#
+#for version in ebixml.findall('{http://www.ebics.org/H000}VersionNumber'):
+#    print ('Protocol:',version.get('ProtocolVersion'),'Version:',version.text)
+
