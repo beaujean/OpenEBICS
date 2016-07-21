ba3697b7d3a0e96acbceb2aecbfe23cd7aa71feb
Aurelien Beaujean <abeaujean@corp.free.fr>
2016-07-21 18:23:35 +0200

    Commit INI and HIA for every users

--- tools/HIA.py
+++ tools/HIA.py
@@ -15,64 +15,70 @@ cfg = OpenEBICS.config()
 
 # Parsing users args
 for user in cfg['Users']:
+
     if 'transport' in cfg['Users'][user]:
         print ('Transporter:',user,'->',cfg['Users'][user]['UserID'])
-        User = user
-        UserID = cfg['Users'][user]['UserID']
+    if 'signature' in cfg['Users'][user]:
+        print ('Signature:',user,'->',cfg['Users'][user]['UserID'])
+    UserID = cfg['Users'][user]['UserID']
 
-# Getting useful certificate informations
-auth_cert = OEcert.get_cert_info('certs/'+User+'/auth.crt')
-encr_cert = OEcert.get_cert_info('certs/'+User+'/crypt.crt')
+    # Getting useful certificate informations
+    auth_cert = OEcert.get_cert_info('certs/'+user+'/auth.crt')
+    encr_cert = OEcert.get_cert_info('certs/'+user+'/crypt.crt')
 
-# 2016-07-07T13:26:01.592+01:00
-TimeStamp = datetime.datetime.now(tz=pytz.timezone('Europe/Paris')).isoformat('T')
+    # 2016-07-07T13:26:01.592+01:00
+    TimeStamp = datetime.datetime.now(tz=pytz.timezone('Europe/Paris')).isoformat('T')
 
-# Opening templates
-TplEnv = Environment(loader=FileSystemLoader('xml/'))
-Tpl_HIA = TplEnv.get_template('HIA.xml')
-Tpl_HIA_data = TplEnv.get_template('HIA-data.xml')
+    # Opening templates
+    TplEnv = Environment(loader=FileSystemLoader('xml/'))
+    Tpl_HIA = TplEnv.get_template('HIA.xml')
+    Tpl_HIA_data = TplEnv.get_template('HIA-data.xml')
 
-# Parsing A005 templates
-xml_HIA_data = Tpl_HIA_data.render(HostID = cfg['Server']['HostID'],
-                        PartnerID = cfg['Server']['PartnerID'],
-                        UserID = UserID,
-                        AuthIssuerName = auth_cert['Issuer'],
-                        AuthSerialNumber = auth_cert['SerialNumber'],
-                        AuthCertificate = auth_cert['Cert'],
-                        AuthModulus = auth_cert['Modulus'],
-                        AuthExponent = auth_cert['Exponent'],
-                        EncrIssuerName = encr_cert['Issuer'],
-                        EncrSerialNumber = encr_cert['SerialNumber'],
-                        EncrCertificate = encr_cert['Cert'],
-                        EncrModulus = encr_cert['Modulus'],
-                        EncrExponent = encr_cert['Exponent'],
-                        TimeStamp = TimeStamp)
-print (xml_HIA_data)
+    # Parsing A005 templates
+    xml_HIA_data = Tpl_HIA_data.render(HostID = cfg['Server']['HostID'],
+                            PartnerID = cfg['Server']['PartnerID'],
+                            UserID = UserID,
+                            AuthIssuerName = auth_cert['Issuer'],
+                            AuthSerialNumber = auth_cert['SerialNumber'],
+                            AuthCertificate = auth_cert['Cert'],
+                            AuthModulus = auth_cert['Modulus'],
+                            AuthExponent = auth_cert['Exponent'],
+                            EncrIssuerName = encr_cert['Issuer'],
+                            EncrSerialNumber = encr_cert['SerialNumber'],
+                            EncrCertificate = encr_cert['Cert'],
+                            EncrModulus = encr_cert['Modulus'],
+                            EncrExponent = encr_cert['Exponent'],
+                            TimeStamp = TimeStamp)
+    #print (xml_HIA_data)
 
-# Gzip and base64 HIA data auth cert
-zip_HIA_data = zlib.compress(xml_HIA_data.encode())
-b64_HIA_data = base64.b64encode(zip_HIA_data)
+    # Gzip and base64 HIA data auth cert
+    zip_HIA_data = zlib.compress(xml_HIA_data.encode())
+    b64_HIA_data = base64.b64encode(zip_HIA_data)
 
-# Parsing HIA templates
-xml_HIA = Tpl_HIA.render(HostID = cfg['Server']['HostID'],
-                        PartnerID = cfg['Server']['PartnerID'],
-                        UserID = UserID,
-                        OrderID = 'A001',
-                        OrderData = b64_HIA_data.decode())
-print (xml_HIA)
+    # Parsing HIA templates
+    xml_HIA = Tpl_HIA.render(HostID = cfg['Server']['HostID'],
+                            PartnerID = cfg['Server']['PartnerID'],
+                            UserID = UserID,
+                            OrderID = 'A001',
+                            OrderData = b64_HIA_data.decode())
+    #print (xml_HIA)
 
-if 'Cert' in cfg['Server']:
-    response = requests.post(cfg['Server']['URL'], cert=cfg['Server']['Cert'], data=xml_HIA.encode())
-else:
-    response = requests.post(cfg['Server']['URL'], xml_HIA.encode())
+    if 'Cert' in cfg['Server']:
+        response = requests.post(cfg['Server']['URL'], cert=cfg['Server']['Cert'], data=xml_HIA.encode())
+    else:
+        response = requests.post(cfg['Server']['URL'], xml_HIA.encode())
+    #print (response.text)
 
-print (response.text)
+    ebixml = ET.fromstring(response.text)
+    ns = {'ebics': 'http://www.ebics.org/H003'}
 
-#ebixml = ET.fromstring(response.text)
-#
-##for child in ebixml:
-##    print (child.tag,' -> ',child.attrib, ' txt: ', child.text)
-#
-#for version in ebixml.findall('{http://www.ebics.org/H000}VersionNumber'):
-#    print ('Protocol:',version.get('ProtocolVersion'),'Version:',version.text)
+    for header in ebixml.findall('ebics:header', ns):
+        for mutable in header.findall('ebics:mutable', ns):
+            ReturnCode = mutable.find('ebics:ReturnCode', ns)
+            print ('\tReturnCode:',ReturnCode.text)
+            ReportText = mutable.find('ebics:ReportText', ns)
+            print ('\tReportText:',ReportText.text)
+    for body in ebixml.findall('ebics:body', ns):
+        ReturnCode = mutable.find('ebics:ReturnCode', ns)
+        print ('\tReturnCode:',ReturnCode.text)
 
--- tools/INI.py
+++ tools/INI.py
@@ -10,6 +10,7 @@ from jinja2 import Environment, FileSystemLoader
 sys.path.append('./libs/')
 import OpenEBICS
 import OpenEBICS.certs as OEcert
+import pprint
 
 cfg = OpenEBICS.config()
 
@@ -18,55 +19,61 @@ TplEnv = Environment(loader=FileSystemLoader('xml/'))
 Tpl_INI = TplEnv.get_template('INI.xml')
 Tpl_A005 = TplEnv.get_template('A005.xml')
 
-# Parsing users args
+# Send an INI request for each user
 for user in cfg['Users']:
+
     if 'transport' in cfg['Users'][user]:
         print ('Transporter:',user,'->',cfg['Users'][user]['UserID'])
-        User = user
-        UserID = cfg['Users'][user]['UserID']
+    if 'signature' in cfg['Users'][user]:
+        print ('Signature:',user,'->',cfg['Users'][user]['UserID'])
+    UserID = cfg['Users'][user]['UserID']
 
-# Getting useful certificate informations
-auth_cert = OEcert.get_cert_info('certs/'+User+'/auth.crt')
+    # Getting useful certificate informations
+    auth_cert = OEcert.get_cert_info('certs/'+user+'/auth.crt')
 
-# 2016-07-07T13:26:01.592+01:00
-TimeStamp = datetime.datetime.now(tz=pytz.timezone('Europe/Paris')).isoformat('T')
+    # 2016-07-07T13:26:01.592+01:00
+    TimeStamp = datetime.datetime.now(tz=pytz.timezone('Europe/Paris')).isoformat('T')
 
-# Parsing A005 templates
-xml_A005 = Tpl_A005.render(HostID = cfg['Server']['HostID'],
-                        PartnerID = cfg['Server']['PartnerID'],
-                        UserID = UserID,
-                        TimeStamp = TimeStamp,
-                        IssuerName = auth_cert['Issuer'],
-                        SerialNumber = auth_cert['SerialNumber'],
-                        Certificate = auth_cert['Cert'],
-                        Modulus = auth_cert['Modulus'],
-                        Exponent = auth_cert['Exponent'])
-print (xml_A005)
+    # Parsing A005 templates
+    xml_A005 = Tpl_A005.render(HostID = cfg['Server']['HostID'],
+                            PartnerID = cfg['Server']['PartnerID'],
+                            UserID = UserID,
+                            TimeStamp = TimeStamp,
+                            IssuerName = auth_cert['Issuer'],
+                            SerialNumber = auth_cert['SerialNumber'],
+                            Certificate = auth_cert['Cert'],
+                            Modulus = auth_cert['Modulus'],
+                            Exponent = auth_cert['Exponent'])
+    #print (xml_A005)
 
-# Gzip and base64 A005 auth cert
-zip_A005 = zlib.compress(xml_A005.encode())
-b64_A005 = base64.b64encode(zip_A005)
+    # Gzip and base64 A005 auth cert
+    zip_A005 = zlib.compress(xml_A005.encode())
+    b64_A005 = base64.b64encode(zip_A005)
 
-# Parsing INI templates
-xml_INI = Tpl_INI.render(HostID = cfg['Server']['HostID'],
-                        PartnerID = cfg['Server']['PartnerID'],
-                        UserID = UserID,
-                        OrderID = 'A001',
-                        OrderData = b64_A005.decode())
-print (xml_INI)
+    # Parsing INI templates
+    xml_INI = Tpl_INI.render(HostID = cfg['Server']['HostID'],
+                            PartnerID = cfg['Server']['PartnerID'],
+                            UserID = UserID,
+                            OrderID = 'A001',
+                            OrderData = b64_A005.decode())
+    #print (xml_INI)
 
-if 'Cert' in cfg['Server']:
-    response = requests.post(cfg['Server']['URL'], cert=cfg['Server']['Cert'], data=xml_INI.encode())
-else:
-    response = requests.post(cfg['Server']['URL'], xml_INI.encode())
+    if 'Cert' in cfg['Server']:
+        response = requests.post(cfg['Server']['URL'], cert=cfg['Server']['Cert'], data=xml_INI.encode())
+    else:
+        response = requests.post(cfg['Server']['URL'], xml_INI.encode())
+    #print (response.text)
 
-print (response.text)
-
-#ebixml = ET.fromstring(response.text)
-#
-##for child in ebixml:
-##    print (child.tag,' -> ',child.attrib, ' txt: ', child.text)
-#
-#for version in ebixml.findall('{http://www.ebics.org/H000}VersionNumber'):
-#    print ('Protocol:',version.get('ProtocolVersion'),'Version:',version.text)
+    ebixml = ET.fromstring(response.text)
+    ns = {'ebics': 'http://www.ebics.org/H003'}
+    
+    for header in ebixml.findall('ebics:header', ns):
+        for mutable in header.findall('ebics:mutable', ns):
+            ReturnCode = mutable.find('ebics:ReturnCode', ns)
+            print ('\tReturnCode:',ReturnCode.text)
+            ReportText = mutable.find('ebics:ReportText', ns)
+            print ('\tReportText:',ReportText.text)
+    for body in ebixml.findall('ebics:body', ns):
+        ReturnCode = mutable.find('ebics:ReturnCode', ns)
+        print ('\tReturnCode:',ReturnCode.text)
 
