7be4586e68620590eb9118c6fff1be31250e2858
Aurelien Beaujean <abeaujean@corp.free.fr>
2016-07-21 17:21:05 +0200

    README.md

--- libs/OpenEBICS/__init__.py
+++ libs/OpenEBICS/__init__.py
@@ -4,14 +4,13 @@ import yaml
 
 # Choose and load config file
 def config():
+
     if sys.argv and sys.argv[1:]:
         cfgfile = sys.argv[1]
     else:
         cfgfile = 'ebics.yml'
 
-
     # Loading the EBICS config file
-    #with open(cfgfile, 'r') as ymlfile:
     try:
         with open(cfgfile, 'r') as ymlfile:
             cfg = yaml.load(ymlfile)
